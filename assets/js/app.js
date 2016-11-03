0dadf60014b44491120577c87417d67c77687836
aungmyohtet <aungmyohtet@outlook.com>
2016-11-03 13:53:56 +0630

    notification added

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,6 +16,28 @@ var UserList = React.createClass({
     this.setState({
       users: users
     });
+
+    // Let's check if the browser supports notifications
+  if (!("Notification" in window)) {
+    alert("This browser does not support desktop notification");
+  }
+
+  // Let's check whether notification permissions have already been granted
+  else if (Notification.permission === "granted") {
+    // If it's okay let's create a notification
+    var notification = new Notification(userName+ " is online now.");
+  }
+
+  // Otherwise, we need to ask the user for permission
+  else if (Notification.permission !== 'denied') {
+    Notification.requestPermission(function (permission) {
+      // If the user accepts, let's create a notification
+      if (permission === "granted") {
+        var notification = new Notification(userName+ " is online now.");
+      }
+    });
+  }
+
   },
   componentWillMount: function() {
     console.log("This was called >> new");
