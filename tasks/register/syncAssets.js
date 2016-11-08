2f28f34719b80e011164db5e51036d7adcdeca1d
aungmyohtet <aungmyohtet@outlook.com>
2016-11-08 09:18:47 +0630

    server side rendering jsx

--- tasks/register/syncAssets.js
+++ tasks/register/syncAssets.js
@@ -13,8 +13,9 @@
 module.exports = function(grunt) {
   grunt.registerTask('syncAssets', [
     'jst:dev',
-    'less:dev',
-    'sync:dev',
-    'coffee:dev'
+  'less:dev',
+  'sync:dev',
+  'coffee:dev',
+  'babel:dev'
   ]);
 };
